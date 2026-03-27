1dcab71553c65d6ebdfabc9cc9fa0c2593f46e8b
michaelhutton86-creator <michael.hutton86@gmail.com>
2026-03-27 10:05:04 +0100

    Fixat Calculator

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,7 @@ public class Calculator {
     public static double sub(double a, double b){
         return a - b; 
     }
+    public static double mul(double a, double b) {
+        return a * b;
 
-}
-
-public static double mul(double a, double b) {
-    return a * b;
 }
