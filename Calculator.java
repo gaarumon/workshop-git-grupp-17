5006b2681885d000543d5e354039af440cec8f69
fatahm06699-glitch <fatahm06699@gmail.com>
2026-03-27 10:36:48 +0100

    Docs: add Javadoc for sub()

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
     public static double add(double a, double b) {
         return a + b;
     }
+    /** Returns the difference of a and b. */ 
     public static double sub(double a, double b){
         return a - b; 
     }
