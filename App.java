834f7a2651f68c033433f5fea392292a82658ede
fatahm06699-glitch <fatahm06699@gmail.com>
2026-03-27 10:24:30 +0100

    Handle choice 2 using sub()

--- App.java
+++ App.java
@@ -4,5 +4,6 @@ public class App{
         System.out.print("Your choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
         if ("1.equals(choice")) System.out.println("TODO Add");
+if ("2".equals(choice)) System.out.println (Calculator.sub(a,b));
     }
 }
